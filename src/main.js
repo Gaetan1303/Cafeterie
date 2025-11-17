c32b5550b5af63e5baead971a0613f7515862663
billy <gaetan.begue13@gmail.com>
2025-11-17 14:22:33 +0100

    prod

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 
 
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
 import { useUserStore } from './store/userStore';
 import { useErrorStore } from './store/errorStore';
 
-
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
 // Activer la persistance du userStore
 const userStore = useUserStore();
-userStore.persist();
+userStore.persist && userStore.persist();
 // Initialiser le errorStore pour la réactivité globale
 useErrorStore();
 app.mount('#app');
